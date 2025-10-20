66a1f678e39e1b96439082fb31a9a51530edd36b
khiem <khiemnguyen.hye@gmail.com>
2025-10-21 02:21:05 +0700

    Change attendee store to object array

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,78 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import './App.css';
 
 const App = () => {
-  const [attendees, setAttendees] = useState({
-    khiem: 1,
-    rich: 1,
-    truc: 1,
-    khoa: 1,
-    tienanh: 1,
-  });
+  const [attendees, setAttendees] = useState([
+    { name: 'khiem', interval: 1 },
+    { name: 'khoa', interval: 1 },
+    { name: 'truc', interval: 1 },
+    { name: 'tienanh', interval: 1 },
+    { name: 'rich', interval: 1 },
+  ]);
 
   type Attendee = keyof typeof attendees;
 
-  const [standingAttendee, setStandingAttendee] = useState<Attendee>();
-
-  const STARTING_MINUTE = 4;
+  const STARTING_MINUTE = 1;
   const [secondsLeft, setSecondsLeft] = useState(STARTING_MINUTE * 60);
 
-  useEffect(() => {
-    const decrementTime = setInterval(() => {
-      setSecondsLeft((prevSeconds) => prevSeconds - 1);
-    }, 1000);
-
-    return () => clearInterval(decrementTime);
-  }, [standingAttendee]);
+  const [standingAttendee, setStandingAttendee] = useState<Attendee>();
 
   useEffect(() => {
     if (!standingAttendee) {
       return;
     }
 
+    const decrementTime = setInterval(() => {
+      setSecondsLeft((prevSeconds) => prevSeconds - 1);
+    }, 100);
+
+    return () => clearInterval(decrementTime);
+  }, [standingAttendee]);
+
+  useEffect(() => {
     if (secondsLeft <= 0) {
-      setAttendees((prev) => ({
-        ...prev,
-        [standingAttendee]: prev[standingAttendee] + 1,
-      }));
+      setAttendees((prev) =>
+        prev.map((a) => (a.name === standingAttendee ? { name: a.name, interval: a.interval + 1 } : a))
+      );
+      new Audio('/interval-over.mp3').play();
       setSecondsLeft(STARTING_MINUTE * 60);
     }
   });
 
   const displayMinute = Math.floor(secondsLeft / 60);
   const displaySecond = secondsLeft % 60;
-  const displayTime = `${displayMinute}:${displaySecond.toString().padStart(2, "0")}`;
+  const displayTime = `${displayMinute}:${displaySecond.toString().padStart(2, '0')}`;
 
   return (
     <>
       <div>
-        <div style={{ display: "flex", gap: "10px" }}>
-          {Object.entries(attendees).map(([name, value]) => (
+        <div style={{fontSize: '180px', fontWeight: 'lighter'}}>{displayTime}</div>
+        <div style={{ display: 'flex', gap: '10px' }}>
+          <button
+            style={standingAttendee === undefined ? {border: '1px solid #747bff'} : {}}
+            onClick={() => {
+              new Audio('/interval-pause.mp3').play();
+              setStandingAttendee(undefined);
+              setSecondsLeft(STARTING_MINUTE * 60);
+            }}
+          ></button>
+          {[...attendees].map((a) => (
             <button
-              key={name}
+              style={a.name === standingAttendee ? { border: '1px solid #747bff' } : {}}
+              key={a.name}
               onClick={() => {
-                setStandingAttendee(name as Attendee);
+                new Audio('/interval-start.mp3').play();
+                setStandingAttendee(a.name as Attendee);
                 setSecondsLeft(STARTING_MINUTE * 60);
               }}
             >
-              {name}: {value}
+              {a.name}: {a.interval}
             </button>
           ))}
         </div>
-        <h3>Countdown timer</h3>
-        <div>Standing: {standingAttendee ? standingAttendee : 'none'}</div>
-        <div>{displayTime}</div>
       </div>
     </>
   );
-}
+};
 
 export default App;
